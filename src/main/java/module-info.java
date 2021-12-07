b51fc9bc6320d431b74a4060dd983710d0a7e9f2
cxii112 <lyakhovoy.d@ya.ru>
2021-12-08 01:48:41 +0500

    jinput dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,8 @@ module me.cxii112.comboard {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
 
+    requires coreapi;
+
     opens me.cxii112.comboard to javafx.fxml;
     exports me.cxii112.comboard;
 }
