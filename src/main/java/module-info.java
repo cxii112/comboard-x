091abe9f062d51bfd611e78e213494709888e9c2
cxii112 <lyakhovoy.d@ya.ru>
2021-12-08 01:47:11 +0500

    boilerplate

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module me.cxii112.comboard {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens me.cxii112.comboard to javafx.fxml;
+    exports me.cxii112.comboard;
+}
